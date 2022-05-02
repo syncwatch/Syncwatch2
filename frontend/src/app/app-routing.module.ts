cad606afd0001b578985aa61875ebe4e7a1bcd8f
skillor <skillor@gmx.net>
2022-05-02 09:51:19 +0200

    fix: no lazy loading

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { AuthGuardService as AuthGuard } from './shared/auth/auth-guard.service';
 import { OnlineGuardService as OnlineGuard } from './shared/sync/online-guard.service';
+// remove this for lazy loading
+import { MoviesModule } from './modules/movies/movies.module';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -14,9 +16,16 @@ const routes: Routes = [
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'downloads', component: DownloadsComponent, canActivate: [AuthGuard] },
     { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard, OnlineGuard] },
+    // lazy loading not really working with Service Worker
+    // {
+    //     path: 'movies',
+    //     loadChildren: () => import('./modules/movies/movies.module').then(m => m.MoviesModule),
+    //     canActivate: [AuthGuard]
+    // },
+    // so we load it normally
     {
         path: 'movies',
-        loadChildren: () => import('./modules/movies/movies.module').then(m => m.MoviesModule),
+        loadChildren: () => MoviesModule,
         canActivate: [AuthGuard]
     },
     { path: '**', redirectTo: '/home' }
