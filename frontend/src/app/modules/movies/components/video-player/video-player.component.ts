60d7a507b3c0a20a8883bfec029a47f3583cf3b0
skillor <skillor@gmx.net>
2022-05-08 13:17:55 +0200

    feat: back to video js

--- frontend/src/app/modules/movies/components/video-player/video-player.component.ts
+++ frontend/src/app/modules/movies/components/video-player/video-player.component.ts
@@ -1,25 +1,17 @@
-import { ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-import { DisplayService } from 'src/app/shared/display/display.service';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MovieMeta } from 'src/app/shared/movie/movie-meta';
 import { MovieService } from 'src/app/shared/movie/movie.service';
+import videojs from 'video.js';
 
 @Component({
     selector: 'app-video-player',
     templateUrl: './video-player.component.html',
     styleUrls: ['./video-player.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
-export class VideoPlayerComponent implements OnInit {
+export class VideoPlayerComponent implements OnInit, OnDestroy {
 
-    @ViewChild('videoContainer', { static: true }) videoContainer!: ElementRef;
     @ViewChild('videoElement', { static: true }) videoElement!: ElementRef;
-    @ViewChild('videoControls', { static: true }) videoControls!: ElementRef;
-    @ViewChild('playPauseButton', { static: true }) playPauseButton!: ElementRef;
-    @ViewChild('progressContainer', { static: true }) progressContainer!: ElementRef;
-    @ViewChild('progress', { static: true }) progress!: ElementRef;
-    @ViewChild('progressBar', { static: true }) progressBar!: ElementRef;
-    @ViewChild('muteButton', { static: true }) muteButton!: ElementRef;
-    @ViewChild('volumeSlider', { static: true }) volumeSlider!: ElementRef;
-    @ViewChild('fullscreenButton', { static: true }) fullscreenButton!: ElementRef;
 
 
     @Input() movie!: MovieMeta;
@@ -28,52 +20,26 @@ export class VideoPlayerComponent implements OnInit {
     alternativeVideos: MovieMeta[] = [];
     subtitles: MovieMeta[] = [];
 
-    video!: HTMLVideoElement;
+    player!: videojs.Player;
 
-    private hasPlayed = false;
     isAutoPlayMuted = true;
 
     constructor(
         private movieService: MovieService,
-        private displayService: DisplayService,
-        private ref: ChangeDetectorRef,
     ) { }
 
     ngOnInit(): void {
-        this.video = this.videoElement.nativeElement;
+        this.player = videojs(this.videoElement.nativeElement, { autoplay: true, controls: true }, () => {
+            console.log('onPlayerReady');
+        });
 
-        this.video.controls = false;
+        this.player.one('play', () => {
+            this.onFirstPlay();
+        });
 
-        this.videoControls.nativeElement.setAttribute('data-state', 'visible');
-
-        if (!this.displayService.canFullscreen()) this.fullscreenButton.nativeElement.style.display = 'none';
-
-        this.video.autoplay = true;
-        this.video.muted = true;
-
-        this.volumeSlider.nativeElement.value = this.video.volume;
-
-        this.video.onplay = (e) => {
-            this.onPlay(e);
-        };
-
-        this.video.onpause = (e) => {
-            this.onPause(e);
-        };
-
-        this.video.onvolumechange = (e) => {
-            this.onVolumeChange(e);
-        };
-
-        this.video.ontimeupdate = (e) => {
-            this.onTimeUpdate(e);
-        };
-
-        this.video.onloadedmetadata = (e) => {
-            this.onLoadedMetaData(e);
-        };
-
-        this.initButtonEvents();
+        this.player.on('play', () => {
+            this.onPlay();
+        });
 
         if (this.movie.sw_type == 'episode') {
             this.movieService.getMovies().subscribe(movies => {
@@ -93,6 +59,7 @@ export class VideoPlayerComponent implements OnInit {
                     }
                 }
                 this.setSource();
+                this.setSource();
             });
             return;
         }
@@ -100,113 +67,38 @@ export class VideoPlayerComponent implements OnInit {
         this.setSource();
     }
 
-    initButtonEvents(): void {
-        this.playPauseButton.nativeElement.onclick = (e: any) => {
-            if (this.video.paused || this.video.ended) this.video.play();
-            else this.video.pause();
-        };
-
-        this.muteButton.nativeElement.onclick = (e: any) => {
-            if (this.video.volume === 0) {
-                this.video.volume = 0.5;
-                this.volumeSlider.nativeElement.value = this.video.volume;
-                this.video.muted = false;
-            } else {
-                this.video.muted = !this.video.muted;
-            }
-        };
-
-        this.fullscreenButton.nativeElement.onclick = (e: any) => {
-            if (this.videoContainer.nativeElement.getAttribute('data-fullscreen') === 'true') {
-                this.displayService.exitFullscreen();
-                this.videoContainer.nativeElement.setAttribute('data-fullscreen', false);
-                this.fullscreenButton.nativeElement.setAttribute('data-state', 'go-fullscreen');
-            } else {
-                this.displayService.requestFullscreen(this.videoContainer.nativeElement);
-                this.videoContainer.nativeElement.setAttribute('data-fullscreen', true);
-                this.fullscreenButton.nativeElement.setAttribute('data-state', 'cancel-fullscreen');
-            }
-        };
-
-        this.progressContainer.nativeElement.onclick = (e: any) => {
-            //var pos = (e.pageX  - this.offsetLeft) / this.offsetWidth; // Also need to take the parent into account here as .controls now has position:relative
-            var pos = (e.pageX  - (this.progressContainer.nativeElement.offsetLeft + this.progressContainer.nativeElement.offsetParent.offsetLeft)) / this.progressContainer.nativeElement.offsetWidth;
-            console.log(pos);
-            this.video.currentTime = pos * this.video.duration;
-        };
-
-        this.volumeSlider.nativeElement.oninput = (e: any) => {
-            this.video.volume = +this.volumeSlider.nativeElement.value;
+    onFirstPlay(): void {
+        this.player.muted(false);
+        if (this.player.paused()) {
+            this.isAutoPlayMuted = true;
+            this.player.muted(true);
+            this.player.play();
         }
+    }
+
+    onPlay(): void {
+        console.log('onplay');
     }
 
     setSource(): void {
-        for (let child of <any>this.video.children) {
-            this.video.removeChild(child);
+        const tracks = this.player.remoteTextTracks();
+        for(let i = tracks.length - 1; i >=0; i--) {
+            this.player.removeRemoteTextTrack(<any>tracks[i]);
         }
-        const source = document.createElement('source');
-        source.src = this.movieService.getMovieStreamUrl(this.originalVideo.id);
-        source.type = this.originalVideo.mime_type!;
-        this.video.appendChild(source);
-
-        for (const alt of this.alternativeVideos) {
-        }
-
-        for (const sub of this.subtitles) {
-            const track = document.createElement('track');
-            track.default = false;
-            track.kind = 'captions';
-            track.srclang = sub.title;
-            track.src = this.movieService.getSubtitleUrl(sub.id);
-            this.video.appendChild(track);
+        this.player!.src(
+            [{ src: this.movieService.getMovieStreamUrl(this.originalVideo.id), type: this.originalVideo.mime_type! }],
+        );
+        for (let sub of this.subtitles) {
+            this.player!.addRemoteTextTrack({
+                src: this.movieService.getSubtitleUrl(sub.id),
+                srclang: sub.title,
+            }, false);
         }
     }
 
-    onFirstPlay(e: Event): void {
-        this.video.muted = false;
-        if (this.video.paused) {
-            this.isAutoPlayMuted = true;
-            this.video.muted = true;
-            this.video.play();
+    ngOnDestroy(): void {
+        if (this.player) {
+            this.player.dispose();
         }
-    }
-
-    onPlayPause(e: Event): void {
-        if (this.video.paused || this.video.ended) {
-            this.playPauseButton.nativeElement.setAttribute('data-state', 'play');
-        } else {
-            this.playPauseButton.nativeElement.setAttribute('data-state', 'pause');
-        }
-    }
-
-    onPlay(e: Event): void {
-        if (!this.hasPlayed) {
-            this.onFirstPlay(e);
-            this.hasPlayed = true;
-        }
-        this.onPlayPause(e);
-    }
-
-    onPause(e: Event): void {
-        this.onPlayPause(e);
-    }
-
-    onVolumeChange(e: Event): void {
-        if (this.video.volume === 0 || this.video.muted) {
-            this.muteButton.nativeElement.setAttribute('data-state', 'unmute');
-        } else {
-            this.muteButton.nativeElement.setAttribute('data-state', 'mute');
-        }
-    }
-
-    onLoadedMetaData(e: Event): void {
-        this.progress.nativeElement.setAttribute('max', this.video.duration);
-    }
-
-    onTimeUpdate(e: Event): void {
-        if (!this.progress.nativeElement.getAttribute('max')) this.progress.nativeElement.setAttribute('max', this.video.duration);
-		this.progress.nativeElement.value = this.video.currentTime;
-		this.progress.nativeElement.style.width = Math.floor((this.video.currentTime / this.video.duration) * 100) + '%';
-        this.ref.detectChanges();
     }
 }
