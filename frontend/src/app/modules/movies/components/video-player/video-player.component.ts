70047e1ddcac015ea001703c53ffc616e878e6cb
skillor <skillor@gmx.net>
2022-05-08 16:56:49 +0200

    feat: player for alt audio

--- frontend/src/app/modules/movies/components/video-player/video-player.component.ts
+++ frontend/src/app/modules/movies/components/video-player/video-player.component.ts
@@ -1,7 +1,7 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MovieMeta } from 'src/app/shared/movie/movie-meta';
 import { MovieService } from 'src/app/shared/movie/movie.service';
-import videojs from 'video.js';
+import videojs, { VideoJsPlayerOptions } from 'video.js';
 
 @Component({
     selector: 'app-video-player',
@@ -12,6 +12,7 @@ import videojs from 'video.js';
 export class VideoPlayerComponent implements OnInit, OnDestroy {
 
     @ViewChild('videoElement', { static: true }) videoElement!: ElementRef;
+    @ViewChild('altVideoElement', { static: true }) altVideoElement!: ElementRef;
 
 
     @Input() movie!: MovieMeta;
@@ -21,24 +22,67 @@ export class VideoPlayerComponent implements OnInit, OnDestroy {
     subtitles: MovieMeta[] = [];
 
     player!: videojs.Player;
+    altPlayer!: videojs.Player;
+    selectedAlternative: MovieMeta | undefined;
 
     isAutoPlayMuted = true;
+    private clickTimeout: any | undefined;
+
+    offsetAltAudio = 0; // value to adjust an offset for the alternative audio
+    private altAudioOffsetStack: number[] = [];
+    private altAudioOffsetStackSize = 10;
+    altAudioAdjustingScore = 0; // a score that goes more positive if it is on a slower device
+    private altAudioAdjustingScoreMax = 1;
+    private altAudioOffsetTolerance = 0.0;
+    private altAudioOffsetToleranceIncrement = 0.03;
+
+    private median(arr: number[], vdefault: number = 0): number {
+        if (arr.length === 0) return vdefault;
+        if (arr.length === 1) return arr[0];
+        const mid = Math.floor(arr.length / 2);
+        const nums = [...arr].sort((a, b) => a - b);
+        return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+    };
 
     constructor(
+        private ref: ChangeDetectorRef,
         private movieService: MovieService,
     ) { }
 
     ngOnInit(): void {
-        this.player = videojs(this.videoElement.nativeElement, { autoplay: true, controls: true }, () => {
-            console.log('onPlayerReady');
+        this.player = videojs(this.videoElement.nativeElement, {
+            userActions: {
+                click: () => { this.onClick() },
+                doubleClick: () => { this.onDoubleClick() },
+            },
+        }, () => {
+            this.onPlayerReady();
         });
 
+        let x: VideoJsPlayerOptions
+
+        this.altPlayer = videojs(this.altVideoElement.nativeElement, {
+
+        }, () => {
+            // console.log('onPlayerReady');
+        });
+    }
+
+    onPlayerReady(): void {
         this.player.one('play', () => {
             this.onFirstPlay();
         });
 
         this.player.on('play', () => {
             this.onPlay();
+        });
+
+        this.player.on('pause', () => {
+            this.onPause();
+        });
+
+        this.player.on('timeupdate', () => {
+            this.onTimeUpdate();
         });
 
         this.player.audioTracks().on('change', () => {
@@ -77,19 +121,6 @@ export class VideoPlayerComponent implements OnInit, OnDestroy {
         this.setSource();
     }
 
-    onFirstPlay(): void {
-        this.player.muted(false);
-        if (this.player.paused()) {
-            this.isAutoPlayMuted = true;
-            this.player.muted(true);
-            this.player.play();
-        }
-    }
-
-    onPlay(): void {
-        console.log('onplay');
-    }
-
     setSource(): void {
         const audioTracks = this.player.audioTracks();
         for (let i = audioTracks.length - 1; i >= 0; i--) {
@@ -113,9 +144,9 @@ export class VideoPlayerComponent implements OnInit, OnDestroy {
 
 
         for (let alt of this.alternativeVideos) {
-            this.player.audioTracks().addTrack(<any>new videojs.AudioTrack({
+            this.player.audioTracks().addTrack(<any>new videojs.AudioTrack(<any>{
                 enabled: false,
-                id: alt.title,
+                id: alt.id,
                 kind: 'translation',
                 label: alt.title,
                 language: alt.title,
@@ -131,7 +162,88 @@ export class VideoPlayerComponent implements OnInit, OnDestroy {
     }
 
     changeAudioTrack(track: videojs.VideojsAudioTrack): void {
-        console.log(track);
+        if (track.id === 'video') { // switch to original sound
+            this.selectedAlternative = undefined;
+            // this.altPlayer.src('');
+            this.altPlayer.pause();
+            // this.altPlayer.dispose();
+        } else { // switch to alternative sound
+            for (let alt of this.alternativeVideos) {
+                if (alt.id === track.id) {
+                    this.selectedAlternative = alt;
+                    this.altPlayer.src(
+                        [{ src: this.movieService.getMovieStreamUrl(alt.id), type: alt.mime_type! }]
+                    );
+                    if (!this.player.paused()) this.altPlayer.play();
+                    return;
+                }
+            }
+        }
+    }
+
+    onClick(): void {
+        if (this.clickTimeout !== undefined) clearTimeout(this.clickTimeout);
+        this.clickTimeout = setTimeout(() => {
+            if (this.player.paused()) {
+                this.player.play();
+            } else {
+                this.player.pause();
+            }
+        }, 500);
+    }
+
+    onDoubleClick(): void {
+        if (this.clickTimeout !== undefined) clearTimeout(this.clickTimeout);
+        if (this.player.isFullscreen()) {
+            this.player.exitFullscreen();
+        } else {
+            this.player.requestFullscreen();
+        }
+    }
+
+    onFirstPlay(): void {
+        this.player.muted(false);
+        if (this.player.paused()) {
+            this.isAutoPlayMuted = true;
+            this.player.muted(true);
+            this.player.play();
+        }
+    }
+
+    onPlay(): void {
+        if (this.selectedAlternative !== undefined) { // update alt player
+            this.altPlayer.play();
+        }
+    }
+
+    onPause(): void {
+        if (this.selectedAlternative !== undefined) { // update alt player
+            this.altPlayer.pause();
+        }
+    }
+
+    onTimeUpdate(): void {
+        if (this.selectedAlternative !== undefined) { // update alt player
+            if (Math.abs(this.altPlayer.currentTime() - this.player.currentTime() + this.offsetAltAudio) > this.altAudioOffsetTolerance) {
+                this.altPlayer.currentTime(this.player.currentTime() - this.offsetAltAudio - this.median(this.altAudioOffsetStack));
+                if (this.altAudioAdjustingScore < this.altAudioAdjustingScoreMax) {
+                    this.altAudioAdjustingScore++;
+                }
+            } else {
+                if (this.altAudioAdjustingScore > -this.altAudioAdjustingScoreMax) {
+                    this.altAudioAdjustingScore--;
+                }
+            }
+            if (this.altAudioAdjustingScore > 0) {
+                this.altAudioOffsetTolerance += this.altAudioOffsetToleranceIncrement;
+            }
+            if (this.altAudioOffsetStack.length >= this.altAudioOffsetStackSize) {
+                this.altAudioOffsetStack.shift();
+            }
+            this.altAudioOffsetStack.push(this.altPlayer.currentTime() - this.player.currentTime() + this.offsetAltAudio);
+
+        }
+        // this.ref.detectChanges();
     }
 
     ngOnDestroy(): void {
